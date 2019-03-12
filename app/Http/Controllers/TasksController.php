24d20b3c24591d6978cb310916a5c289b90646c4
iefuji <iefuji@carebank.systems>
2019-03-12 10:24:41 +0000

    error_fix

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -52,7 +52,7 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'status' => 'required|max:10',
-            'content' => 'required|max:10',
+            'content' => 'required|max:191',
         ]);
         
         $task = new Task;
@@ -110,7 +110,7 @@ class TasksController extends Controller
     {
         $this->validate($request, [
             'status' => 'required|max:10',
-            'content' => 'required|max:10',
+            'content' => 'required|max:191',
         ]);
         
         $task = Task::find($id);
